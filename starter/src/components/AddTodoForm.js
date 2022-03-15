cfeeb13cd26ebecda02c70a13d50dbbc99915e80
phan-ba-duy <phan-ba-duy@com-way.co.jp>
2022-03-15 11:27:49 +0900

    redux practices without api call

--- starter/src/components/AddTodoForm.js
+++ starter/src/components/AddTodoForm.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../redux/todoSlice';
 
 const AddTodoForm = () => {
 	const [value, setValue] = useState('');
+	const dispatch = useDispatch();
 
 	const onSubmit = (event) => {
 		event.preventDefault();
-		console.log('user entered: ' + value);
+		if (value) {
+			dispatch(
+				addTodo({
+					title: value,
+				})
+			)
+		}
 	};
 
 	return (
--- starter/src/components/TotalCompleteItems.js
+++ starter/src/components/TotalCompleteItems.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const TotalCompleteItems = () => {
-	return <h4 className='mt-3'>Total Complete Items: 5</h4>;
+	const todos = useSelector((state) => 
+		state.todos.filter((todo) => todo.completed === true)
+	);
+
+	return <h4 className='mt-3'>Total Complete Items: {todos.length}</h4>;
 };
 
 export default TotalCompleteItems;
